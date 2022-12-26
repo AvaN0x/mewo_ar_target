c605d58b39ba5ae2b044d50295ab88e9cb237cb8
AvaN0x <clement.ricatte57@gmail.com>
2022-12-26 17:55:09 +0100

    feat: :sparkles: add animation to points

--- components/object/Point.vue
+++ components/object/Point.vue
@@ -4,7 +4,10 @@
       :value="`+${value}`"
       position="0 0 0.02"
       :color="color"
-      scale="1.2 1.2 1.2"
+      scale="1.5 1.5 1.5"
+      align="center"
+      animation__pos="property: position; to: 0 2 0.02; dur: 1000; easing: easeInQuad"
+      animation__scale="property: scale; to: 0 0 0; dur: 1000; easing: easeInQuad"
     ></a-text>
   </a-entity>
 </template>
